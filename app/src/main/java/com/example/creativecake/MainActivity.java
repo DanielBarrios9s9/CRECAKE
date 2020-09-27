b8d59b854e347401804a710ff7d592e0334a430c
JuanPabloCarrilloAcero <jcarrilloa@unal.edu.co>
2020-09-26 22:32:56 -0500

    26/09/2020 Eliminar la carpeta ui/main ya que no está siendo utilizada, editar el archivo MainActivity.java para que las importaciones fueran solo las necesarias JuanPabloCarrilloAcero

--- app/src/main/java/com/example/creativecake/MainActivity.java
+++ app/src/main/java/com/example/creativecake/MainActivity.java
@@ -1,23 +1,6 @@
 package com.example.creativecake;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import com.example.creativecake.ui.main.SectionsPagerAdapter;
-
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
