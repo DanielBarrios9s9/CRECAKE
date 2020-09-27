43c24b41d99cfb3653f1f039b0fe49cf950d8d66
JuanPabloCarrilloAcero <jcarrilloa@unal.edu.co>
2020-09-27 13:07:58 -0500

    27/09/2020 Modificar Menu_provisional.java para añadair la funcionalidad de pasar a las pantalla dependiendo de la opción seleccionada, modificar la interfaz de activity_menu_provisional.xml JuanPabloCarrilloAcero

--- app/src/main/java/com/example/creativecake/Menu_provisional.java
+++ app/src/main/java/com/example/creativecake/Menu_provisional.java
@@ -1,13 +1,51 @@
 package com.example.creativecake;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Menu_provisional extends AppCompatActivity {
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private Button siguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_provisional);
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        siguiente = (Button) findViewById(R.id.siguiente);
+
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+
+                radioButton = (RadioButton) findViewById(selectedId);
+                System.out.println(selectedId);
+                // 2131230920 cliente
+                // 2131230921 tienda
+                // 2131230922 domicilio
+                if (selectedId == 2131230920){
+                    Intent siguiente = new Intent(Menu_provisional.this, Inicio_cliente.class);
+                    startActivity(siguiente);
+                } else if (selectedId == 2131230921){
+                    System.out.println("Tienda");
+                } else if (selectedId == 2131230922){
+                    System.out.println("Domiciliario");
+                }
+            }
+        });
     }
 }
