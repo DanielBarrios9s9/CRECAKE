31cb9f180fa1b5bb32ea27ab7a3780450a259710
Paula2208 <71159016+paula2208@users.noreply.github.com>
2020-09-28 21:50:30 -0500

    Se crea la interfaz de catálogo con algunos ejemplos.

--- app/src/main/java/com/example/creativecake/RegistroNegocio.java
+++ app/src/main/java/com/example/creativecake/RegistroNegocio.java
@@ -1,14 +1,14 @@
 package com.example.creativecake;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class RegistroNegocio extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registro_negocio);
     }
 }
