8ed99135a755b9d83c72e863d6b02be998ba3dab
Paula2208 <71159016+paula2208@users.noreply.github.com>
2020-09-29 21:08:18 -0500

    item

---
+++ app/src/main/java/com/example/creativecake/Ofertas_cliente.java
@@ -0,0 +1,14 @@
+package com.example.creativecake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ofertas_cliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ofertas_cliente);
+    }
+}
