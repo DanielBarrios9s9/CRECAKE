95e1f479b0eb0fcd30ef0ba5a606d72d107c8401
Daniel1309-gon <71108797+Daniel1309-gon@users.noreply.github.com>
2020-10-06 17:22:40 -0500

    añado codigo de registro con firebase

--- app/src/main/java/com/example/creativecake/MainActivity.java
+++ app/src/main/java/com/example/creativecake/MainActivity.java
@@ -7,6 +7,9 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button iniciaSesion;
--- app/src/main/java/com/example/creativecake/RegistroCliente.java
+++ app/src/main/java/com/example/creativecake/RegistroCliente.java
@@ -3,12 +3,43 @@ package com.example.creativecake;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class RegistroCliente extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegistroCliente extends AppCompatActivity
+{
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    private EditText  etNombre, etCorreo, etPassword, etTelefono, etDireccion, etEdad;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_cliente);
+
+        etNombre = (EditText)findViewById(R.id.idNombre);
+        etCorreo = (EditText)findViewById(R.id.idCorreo);
+        etPassword = (EditText)findViewById(R.id.idPassword);
+        etTelefono = (EditText)findViewById(R.id.idTelefono);
+        etDireccion = (EditText)findViewById(R.id.idDireccion);
+        etEdad = (EditText)findViewById(R.id.idEdad);
+
+
+        // Write a message to the database
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("usuarioCliente");
+
+        String nombre = etNombre.getText().toString();
+        String correo = etCorreo.getText().toString();
+        String password = etPassword.getText().toString();
+        String telefono = etTelefono.getText().toString();
+        String direccion = etDireccion.getText().toString();
+        String edad = etEdad.getText().toString();
+
+        UserHelperClass helperClass = new UserHelperClass(nombre, correo, password,telefono, direccion, edad);
+        reference.setValue(helperClass);
     }
 }
