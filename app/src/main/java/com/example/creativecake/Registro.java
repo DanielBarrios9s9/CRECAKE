fe5b74b526b3221b293b113e0fb23e4e64cd9a72
Daniel1309-gon <71108797+Daniel1309-gon@users.noreply.github.com>
2020-10-11 17:50:05 -0500

    Añado codigo de manejo de radiobuttons en activiy registro

--- app/src/main/java/com/example/creativecake/Registro.java
+++ app/src/main/java/com/example/creativecake/Registro.java
@@ -2,13 +2,39 @@ package com.example.creativecake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
-public class Registro extends AppCompatActivity {
+public class Registro extends AppCompatActivity
+{
+    private RadioButton rbCliente, rbNegocio, rbDomiciliario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        rbCliente = (RadioButton)findViewById(R.id.idRbCliente);
+        rbNegocio = (RadioButton)findViewById(R.id.idRbNegocio);
+        rbDomiciliario = (RadioButton)findViewById(R.id.idRbDomiciliario);
+    }
+
+    public void SiguienteActivity(View view)
+    {
+        if (rbCliente.isChecked() == true)
+        {
+            Intent intent = new Intent(getApplicationContext(), RegistroCliente.class);
+            startActivity(intent);
+        }else if (rbNegocio.isChecked() == true)
+        {
+            Intent intent = new Intent(getApplicationContext(), RegistroNegocio.class);
+            startActivity(intent);
+        }else if (rbDomiciliario.isChecked()== true)
+        {
+            Intent intent = new Intent(getApplicationContext(), Registro_domiciliario.class);
+            startActivity(intent);
+        }
     }
 }
