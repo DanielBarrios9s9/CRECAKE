fdc999a428456763e7b0a05ecad67173681029e2
Daniel1309-gon <71108797+Daniel1309-gon@users.noreply.github.com>
2020-12-06 23:38:11 -0500

    correccion en cotizaciones y un hint en layout de administrador

--- app/src/main/java/com/example/creativecake/CotizacionClienteFragment.java
+++ app/src/main/java/com/example/creativecake/CotizacionClienteFragment.java
@@ -99,7 +99,29 @@ public class CotizacionClienteFragment extends Fragment {
                 datosUsuario.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        usuario =snapshot.getValue(UserHelperClass.class);
+                        String direccionFROMDB = snapshot.child("direccion").getValue(String.class);
+                        String telefonoFROMDB = snapshot.child("telefono").getValue(String.class);
+                        String nombreFROMDB = snapshot.child("nombre").getValue(String.class);
+
+                        String nombre_ = nombre_cotizacion.getText().toString();
+                        String decoracion_ = decoración.getSelectedItem().toString();
+                        String sabor_ = sabor.getSelectedItem().toString();
+                        String cobertura_ = cobertura.getSelectedItem().toString();
+                        String tamaño_ = tamaño.getSelectedItem().toString();
+                        String precio_ = precio.getText().toString();
+                        String fecha_ = fecha.getText().toString();
+                        String hora_ = hora.getText().toString();
+                        String especificaciones_ = especificaciones.getText().toString();
+
+                        database = FirebaseDatabase.getInstance();
+                        reference = database.getReference("cotizaciones");
+
+                        final CotizacionHelperClass helperClass = new CotizacionHelperClass(nombre_,
+                                decoracion_, sabor_, cobertura_, tamaño_, precio_, fecha_, hora_,direccionFROMDB
+                                , telefonoFROMDB, nombreFROMDB,
+                                especificaciones_,"PENDIENTE");
+
+                        reference.child(telefono).setValue(helperClass);
                     }
 
                     @Override
@@ -110,7 +132,7 @@ public class CotizacionClienteFragment extends Fragment {
 
 
 
-                String nombre_ = nombre_cotizacion.getText().toString();
+               /* String nombre_ = nombre_cotizacion.getText().toString();
                 String decoracion_ = decoración.getSelectedItem().toString();
                 String sabor_ = sabor.getSelectedItem().toString();
                 String cobertura_ = cobertura.getSelectedItem().toString();
@@ -125,10 +147,10 @@ public class CotizacionClienteFragment extends Fragment {
 
                 final CotizacionHelperClass helperClass = new CotizacionHelperClass(nombre_,
                         decoracion_, sabor_, cobertura_, tamaño_, precio_, fecha_, hora_,
-                        usuario.getDireccion(), usuario.getTelefono(), usuario.getNombre(),
+                        , usuario.getTelefono(), usuario.getNombre(),
                         especificaciones_,"PENDIENTE");
 
-                reference.child(telefono).push().setValue(helperClass);
+                reference.child(telefono).setValue(helperClass);*/
 
 
 
